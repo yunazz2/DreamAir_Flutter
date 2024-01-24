0cb277e9190c64f14a416f6bf322994cbe5a6fa4
yunazz2 <z____yn@naver.com>
2024-01-24 18:46:39 +0900

    ⛄

--- dreamair_flutter/linux/flutter/generated_plugins.cmake
+++ dreamair_flutter/linux/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_linux
+  flutter_secure_storage_linux
   nb_utils
   smart_auth
 )
--- dreamair_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dreamair_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import connectivity_plus
 import file_selector_macos
+import flutter_secure_storage_macos
 import nb_utils
 import path_provider_foundation
 import photo_manager
@@ -16,6 +17,7 @@ import smart_auth
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
   NbUtilsPlugin.register(with: registry.registrar(forPlugin: "NbUtilsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
